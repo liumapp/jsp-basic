3eba88c1164ee1e3ae3a7f82a5600f40409e6ada
liumapp <liumapp.com@gmail.com>
2017-06-28 22:37:58 +0800

    raf detail

--- src/main/java/com/liumapp/jspbasic/io/RafDemo.java
+++ src/main/java/com/liumapp/jspbasic/io/RafDemo.java
@@ -3,6 +3,7 @@ package com.liumapp.jspbasic.io;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Arrays;
 
 /**
  * Created by liumapp on 6/28/17.
@@ -24,7 +25,41 @@ public class RafDemo {
 
         RandomAccessFile raf = new RandomAccessFile(file , "rw");
         System.out.println(raf.getFilePointer());
+        raf.write('A');
+        System.out.println(raf.getFilePointer());
+        raf.write('B');
+        System.out.println(raf.getFilePointer());
 
+        int i = 0x7fffffff;//8位的16进制数，每两位就要占用一个字节
+        System.out.println(i);
+        //用write方法每次只能写一个字节，如果要把i写进去需要分四次写
+        raf.write( i >>> 24);//高8位
+        raf.write( i >>> 16);
+        raf.write( i >>> 8);
+        raf.write( i);
+        System.out.println(raf.getFilePointer());
+
+        //可以直接写一个int类型的数，但其底层的实现跟上面的写法一样
+        raf.writeInt(i);
+        System.out.println(raf.getFilePointer());
+
+        String s = "中";
+        byte[] utf8 = s.getBytes("utf-8");//编码上，utf-8中，一个汉字占3个字节
+        raf.write(utf8);
+        System.out.println(raf.getFilePointer());
+
+        //读文件，把指针移到头部开始读
+        raf.seek(0);
+        //把文件内容一次性读取到字节数组中
+        byte[] buf =  new byte[(int)raf.length()];
+        raf.read(buf);
+        System.out.println(Arrays.toString(buf));
+
+        /**
+         * 这样的输出方式将会导致乱码
+         */
+//        String s1 = new String(buf , "utf-16");
+//        System.out.println(s1);
     }
 
 }
