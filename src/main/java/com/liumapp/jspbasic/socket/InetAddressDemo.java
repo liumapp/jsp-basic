aa7104ae8641fd46935e0a05a95d8d8f7dee4172
liumapp <liumapp.com@gmail.com>
2017-07-03 19:15:25 +0800

    add example

--- src/main/java/com/liumapp/jspbasic/socket/InetAddressDemo.java
+++ src/main/java/com/liumapp/jspbasic/socket/InetAddressDemo.java
@@ -16,6 +16,16 @@ public class InetAddressDemo {
         System.out.println("计算机名：" + address.getHostName());
         System.out.println("IP地址：" + address.getHostAddress());
 
+        byte[] ipArray = address.getAddress();
+        System.out.println("已字节数组形式返回的ip：" + ipArray);
+
+        System.out.println("直接返回对象：" + address);
+
+        InetAddress address2 = InetAddress.getByName("106.14.212.41");//获取网络上的计算机信息
+        System.out.println("网络上的一台计算机名称：" + address2.getHostName());
+        System.out.println("它的IP地址：" + address2.getHostAddress());
+
+
     }
 
 }
