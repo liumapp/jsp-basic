855204da974225238e1c116e84ea8137d5e1e117
liumapp <liumapp.com@gmail.com>
2017-07-06 09:59:26 +0800

    udp socket

--- src/main/java/com/liumapp/jspbasic/socket/UDPClient.java
+++ src/main/java/com/liumapp/jspbasic/socket/UDPClient.java
@@ -1,10 +1,63 @@
 package com.liumapp.jspbasic.socket;
 
+import java.io.IOException;
+import java.net.*;
+
 /**
  * Created by liumapp on 7/4/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
 public class UDPClient {
+
+    public static void main (String[] args) throws IOException {
+        /**
+         * 定义服务器的地址
+         * 端口号
+         * 数据
+         */
+        InetAddress address = InetAddress.getByName("localhost");
+        int port = 9999;
+        byte[] data = "用户名：admin;密码：123".getBytes();
+
+        /**
+         * 创建数据报
+         * 包含发送的数据信息
+         */
+        DatagramPacket packet = new DatagramPacket(data , data.length , address , port);
+
+        /**
+         * 创建DatagramSocket
+         */
+        DatagramSocket socket = new DatagramSocket();
+
+        /**
+         * 向服务器端发送数据报
+         */
+        socket.send(packet);
+
+        /**
+         * 接受server端响应的数据
+         * 创建一个数据报用于接受数据
+         */
+        byte[] data2 = new byte[1024];
+        DatagramPacket packet2 = new DatagramPacket(data2 , data2.length);
+        /**
+         * 接受服务器响应的数据
+         */
+        socket.receive(packet2);
+        /**
+         * 读取数据
+         */
+        String reply = new String(data2 , 0 , packet2.getLength());
+        System.out.println("client , get data : " + reply);
+
+        /**
+         * 关闭资源
+         */
+        socket.close();
+
+
+    }
 
 }
--- src/main/java/com/liumapp/jspbasic/socket/UDPServer.java
+++ src/main/java/com/liumapp/jspbasic/socket/UDPServer.java
@@ -1,5 +1,11 @@
 package com.liumapp.jspbasic.socket;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /**
  * Created by liumapp on 7/4/17.
  * E-mail:liumapp.com@gmail.com
@@ -7,5 +13,49 @@ package com.liumapp.jspbasic.socket;
  * 实现基于UDP的用户登录
  */
 public class UDPServer {
-    
+
+    public static void main (String[] args) throws IOException {
+        /**
+         * 接受client数据
+         */
+        DatagramSocket socket = new DatagramSocket(9999);
+        byte[] data = new byte[20 * 1024];
+        DatagramPacket packet = new DatagramPacket(data , data.length);
+
+        /**
+         * 接受客户端的数据
+         * 在接收到数据之前会一直处于阻塞状态
+         */
+        System.out.println("server即将启动");
+        socket.receive(packet);
+
+        String info = new String(data , 0 , packet.getLength());
+        System.out.println("server data : " + info);
+
+        /**
+         * 向client响应数据
+         * 定义客户端的地址、端口号和数据
+         */
+        InetAddress address = packet.getAddress();
+        int port = packet.getPort();
+        byte[] data2 = "欢迎您" . getBytes();
+
+        /**
+         * 创建数据报
+         * 包含响应的数据信息
+         */
+        DatagramPacket packet2 = new DatagramPacket(data2 , data2.length ,  address , port);
+
+        /**
+         * 响应客户端
+         */
+        socket.send(packet2);
+
+        /**
+         * 关闭资源
+         */
+        socket.close();
+
+    }
+
 }
